f1d102c4fd2bade712b4e760936669d64f93ef4f
Jake Mitchell <mitchellja97@gmail.com>
2018-12-27 00:11:22 -0600

    move

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   helpers.checkValidInternetConnection()
     .then(() => {
-      res.sendFile(path.join(__dirname + '/index.html'))
+      res.sendFile(path.join(__dirname + '/public/index.html'))
     })
     .catch((err) => {
       console.log(err)
