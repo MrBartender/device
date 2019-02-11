8b1ae28ce5bf211659935e48e8b8712cbf8ba7a6
Jake Mitchell <mitchellja97@gmail.com>
2019-02-10 19:12:04 -0500

    activate pumps

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const helmet = require('helmet')
 const bodyParser = require('body-parser')
 
-// const gpio = require('onoff').Gpio
+const gpio = require('onoff').Gpio
 const isOnline = require('is-online')
 const wifi = require('node-wifi')
 
@@ -15,18 +15,18 @@ const app = express()
 let nums = [ 2, 3, 4, 5, 6, 13, 14, 15, 17, 18, 19, 26 ]
 
 let pumps = {
-  // '1': new gpio(26, 'high'),
-  // '2': new gpio(19, 'high'),
-  // '3': new gpio(13, 'high'),
-  // '4': new gpio(6, 'high'),
-  // '5': new gpio(5, 'high'),
-  // '6': new gpio(2, 'high'),
-  // '7': new gpio(3, 'high'),
-  // '8': new gpio(4, 'high'),
-  // '9': new gpio(17, 'high'),
-  // '10': new gpio(14, 'high'),
-  // '11': new gpio(15, 'high'),
-  // '12': new gpio(18, 'high')
+  '1': new gpio(26, 'high'),
+  '2': new gpio(19, 'high'),
+  '3': new gpio(13, 'high'),
+  '4': new gpio(6, 'high'),
+  '5': new gpio(5, 'high'),
+  '6': new gpio(2, 'high'),
+  '7': new gpio(3, 'high'),
+  '8': new gpio(4, 'high'),
+  '9': new gpio(17, 'high'),
+  '10': new gpio(14, 'high'),
+  '11': new gpio(15, 'high'),
+  '12': new gpio(18, 'high')
 }
 
 // Init the wifi module
